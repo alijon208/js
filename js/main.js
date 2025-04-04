213845539d876ffb09ea120325cbd3546290b7b8
alijon208 <andurahmonovmuhammadali@gmail.com>
2025-04-04 20:14:51 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/* 4 */
+
+// const a = 3.14;
+// const d = Number(prompt("Sonni kiriting:"));
+// const l = a*d;
+// alert("Aylananing uzunligi " + l + " ga teng")
+
+
+/* 5 */
+
+// const a = Number(prompt("Sonni kiriting:"));
+// const v = a*a*a;
+// const s = 6*(a*a);
+// alert("Kubning hajmi " + v + " ga teng");
+// alert("Kubning To'la sirti " + s + " ga teng");
+
+
+/* 6 */ 
+
+// const a = Number(prompt("Birinchi sonni kiriting:"));
+// const b = Number(prompt("Ikkinchi sonni kiriting:"));
+// const c = Number(prompt("Uchinchi sonni kiriting:"))
+// const v = a*b*c;
+// const s = 2*(a*b+b*c+a*c);
+// alert("Paralellopipedning hajmi " + v + " ga teng")
+// alert("Paralellopipedning to'la sirti " + s + " ga teng");
+
+
+/* 8 */
+
+// const r = Number(prompt("Sonni kiriting:"))
+// const pi = 3.14;
+// const l = 2*pi*r;
+// const s = pi*(r*r);
+// alert("Doiraning uzunligi " + l + " ga teng")
+// alert("Doiraning yuzasi " + s + " ga teng");
+
+
+/* 9 */
+
+// const a = Number(prompt("Birinchi sonni kiriting:"));
+// const b = Number(prompt("Ikkinchi sonni kiriting:"));
+// const value = (a+b)/2;
+// alert("Sonning o'rta arifmetigi " + value + " ga teng")
+
+
+/* 10 */
+
+// const a = Number(prompt("Birinchi sonni kiriting:"));
+// const b = Number(prompt("Ikkinchi sonni kiriting:"));
+// const yigindi = a+b;
+// const kopaytma = a*b;
+// const kvadrat1 = a*a;
+// const kvadrat2 = b*b;
+// alert("Sonlarning yig'indisi " + yigindi + " ga teng");
+// alert("Sonlarning ko'paytmasi " + kopaytma + " ga teng");
+// alert("Sonning birinchisini kvadrati " + kvadrat1 + " ga teng");
+// alert("Sonning ikkinchisini kavadrati " + kvadrat2 + " ga teng");
+
+/* 11 */
+
+// const a = Number(prompt("Birinchi sonni kiriting:"));
+// const b = Number(prompt("Ikkinchi sonni kiriting:"));
+// const yigindi = a+b;
+// const kopaytma = a*b;
+// const modul1 = a*-1;
+// const modul2 = b*-1;
+// alert("Sonlarning yig'indisi " + yigindi + " ga teng");
+// alert("Sonlarning ko'paytmasi " + kopaytma + " ga teng");
+// alert("Sonning birinchisini moduli " + modul1 + " ga teng");
+// alert("Sonning ikkinchisini moduli " + modul2 + " ga teng");
